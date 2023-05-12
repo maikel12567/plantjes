4ec6c33419598af8a9f44dd27fc34eb8da86ef69
maikel12567 <115786281+maikel12567@users.noreply.github.com>
2023-05-12 10:21:00 +0200

    made more pages and header

--- header.php
+++ header.php
@@ -1,3 +1,10 @@
 <header>
-    
+    <img src="" alt="logo">
+    <input type="search">
+    <nav>
+        <a href="index.php">home</a>
+        <a href="">planten</a>
+        <a href="">dieren</a>
+        <a href="">vijver</a>
+    </nav>
 </header>
