f9294e69a96d5d9b3d511473919e0eda2e329595
maikel12567 <115786281+maikel12567@users.noreply.github.com>
2023-05-12 10:07:38 +0200

    made header

--- index.php
+++ index.php
@@ -1,32 +1,12 @@
 <!doctype html>
-<html class="no-js" lang="">
-
-<head>
-  <meta charset="utf-8">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <meta property="og:title" content="">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
-
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
-
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
-
-  <meta name="theme-color" content="#fafafa">
-</head>
-
+<html class="no-js" lang="nl">
+<?php require_once 'head.php' ?>
 <body>
+  <?php require_once 'header.php' ?>
 
   <!-- Add your site or application content here -->
   <p>Hello world! This is HTML5 Boilerplate.</p>
-  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/vendor/modernizr-3.11.2.min.js"></script>s
   <script src="js/plugins.js"></script>
   <script src="js/main.js"></script>
 
